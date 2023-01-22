03d098aa98142274362ec06cd53eec64d23b5d4f
Tuchix10 <tuchix10@gmail.com>
2023-01-22 20:24:14 -0300

    Configurando busqueda

--- config.js
+++ config.js
@@ -28,9 +28,12 @@ function validarAño(anno) {
 }
 
 function registrarLibro() {
-    let titulo = document.getElementById("titulo").value;
-    let autor = document.getElementById("autor").value;
-    let editorial = document.getElementById("editorial").value;
+    let tituloNormal = document.getElementById("titulo").value;
+    let titulo = tituloNormal.toLowerCase();
+    let autorNormal = document.getElementById("autor").value;
+    let autor = autorNormal.toLowerCase();
+    let editorialNormal = document.getElementById("editorial").value;
+    let editorial = editorialNormal.toLowerCase();
     let anno = document.getElementById("año").value;
     let codigo = document.getElementById("codigo").value;
     if (validarAño(anno) && validarFormulario(titulo , autor , editorial , anno , codigo)) {
@@ -52,15 +55,78 @@ function agregarFila() {
     }
 }
 
-function generarTabla () {
+function generarTabla() {
     tab.innerHTML = "";
     agregarFila();
 }
 
-function consultar() {
-    console.log(libreria);
+function validarBusqueda(titulo , autor , editorial , anno , codigo) {
+    if (titulo) {
+        let libreriaFiltrada = libreria.filter(Libro => Libro.titulo === titulo);
+        if (libreriaFiltrada.length === 0) {
+            alert(`Su busqueda no produjo resultados`);
+        }
+        generarTablaBusqueda(libreriaFiltrada)
+        return true
+    }
+    if (autor) {
+        let libreriaFiltrada = libreria.filter(Libro => Libro.autor === autor);
+        if (libreriaFiltrada.length === 0) {
+            alert(`Su busqueda no produjo resultados`);
+        }
+        generarTablaBusqueda(libreriaFiltrada)
+        return true
+    }
+    if (editorial) {
+        let libreriaFiltrada = libreria.filter(Libro => Libro.editorial === editorial);
+        if (libreriaFiltrada.length === 0) {
+            alert(`Su busqueda no produjo resultados`);
+        }
+        generarTablaBusqueda(libreriaFiltrada)
+        return true
+    }
+    if (anno) {
+        let libreriaFiltrada = libreria.filter(Libro => Libro.anno === anno);
+        if (libreriaFiltrada.length === 0) {
+            alert(`Su busqueda no produjo resultados`);
+        }
+        generarTablaBusqueda(libreriaFiltrada)
+        return true
+    }
+    if (codigo) {
+        let libreriaFiltrada = libreria.filter(Libro => Libro.codigo === codigo);
+        if (libreriaFiltrada.length === 0) {
+            alert(`Su busqueda no produjo resultados`);
+        }
+        generarTablaBusqueda(libreriaFiltrada)
+        return true
+    }
+    alert(`Por favor ingrese algún parametro para la busqueda`);
+    return false;
 }
 
+function busqueda() {
+    let tituloNormal = document.getElementById("titulo-busqueda").value;
+    let titulo = tituloNormal.toLowerCase();
+    let autorNormal = document.getElementById("autor-busqueda").value;
+    let autor = autorNormal.toLowerCase();
+    let editorialNormal = document.getElementById("editorial-busqueda").value;
+    let editorial = editorialNormal.toLowerCase();
+    let anno = document.getElementById("año-busqueda").value;
+    let codigo = document.getElementById("codigo-busqueda").value;
+    validarBusqueda(titulo , autor , editorial , anno , codigo);
+}
 
-
-
+function generarTablaBusqueda(libreriaFiltrada) {
+    tab.innerHTML = "";
+    for( let libro of libreriaFiltrada) {
+        tab.innerHTML = tab.innerHTML +
+        `<tr>
+            <td>${(libro.titulo)}</td>
+            <td>${(libro.autor)}</td>
+            <td>${(libro.editorial)}</td>
+            <td>${(libro.anno)}</td>
+            <td>${(libro.codigo)}</td>
+        </tr>`;
+    }
+}
